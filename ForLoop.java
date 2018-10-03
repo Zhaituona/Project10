1f907c26d680f1a3fef67e67fb7bf16862b78f29
Zhaituona <42330994+Zhaituona@users.noreply.github.com>
2018-10-03 00:09:46 -0400

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int k = 0; k < 20; k +=2){
+            System.out.println(k);
+        }
+    }
+}
+//for loop looks like
+//for(initialization block ; terminator block ; increment/deceremnt){open body
+
+//}close body
