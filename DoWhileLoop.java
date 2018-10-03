7e6f089d62584ae453fa8c6c624737f61d262b70
Zhaituona <42330994+Zhaituona@users.noreply.github.com>
2018-10-03 00:08:59 -0400

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,15 @@
+package Loop;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int m = 12;
+        do {
+            System.out.println(m);
+            m++;
+        }
+            while(m <10);
+
+    }
+}// do while loop atleast do one time the test before the condition .
+//inisializaion block  then do {open boday   do some output , then increment or decrement ,  then is erminator
+//block in to while .
