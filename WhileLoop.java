726b2f38a62b6b909cdfdc890aeb05c8937b778d
Zhaituona <42330994+Zhaituona@users.noreply.github.com>
2018-10-03 00:10:29 -0400

    Add files via upload
    Difference between while loop and do while loop ,

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int n = 0;
+        while(n <=15){
+    System.out.println(n);
+    n ++;
+        }
+    }
+}// while loop looks like , first initalization block , then while(terminator block){ then while body}
+//then incerecement or decrecement .
